86184ad0cb110ee0488ae59c7bc7c79c67ad8532
brano2 <b.pilnan@gmail.com>
2017-04-22 11:29:45 +0000

    Allowing faster readings for MPU6050 Now possible to retrieve all the readings (accl, temperature, gyro) in a single I2C transaction. Also added structs for storing the readings.

--- drivers/mpu6050.cpp
+++ drivers/mpu6050.cpp
@@ -17,89 +17,96 @@ using namespace std;
 
 Mpu6050::Mpu6050(I2C *bus)
 {
-    this->bus = bus;
-    init();
+  this->bus = bus;
+  init();
 }
 
 void Mpu6050::init() {
-    // POWER MGMT setup
-    char buf[2];
-    buf[0] = 0x6B; buf[1] = 0x80;
-    this->bus->write(slave_addr, 2, buf); //PWR_MGMT_1 - reset
-    sleep(1);
-
-    buf[0] = 0x6B; buf[1] = 0x03;
-    this->bus->write(slave_addr, 2, buf); //PWR_MGMT_1 - set gyro z as clock source
-    buf[0] = 0x6C; buf[1] = 0;
-    this->bus->write(slave_addr, 2, buf); //PWR_MGMT_2
-    sleep(1);
+  // POWER MGMT setup
+  char buf[2];
+  buf[0] = 0x6B; buf[1] = 0x80;
+  this->bus->write(slave_addr, 2, buf); //PWR_MGMT_1 - reset
+  sleep(1);
+  
+  buf[0] = 0x6B; buf[1] = 0x03;
+  this->bus->write(slave_addr, 2, buf); //PWR_MGMT_1 - set gyro z as clock source
+  buf[0] = 0x6C; buf[1] = 0;
+  this->bus->write(slave_addr, 2, buf); //PWR_MGMT_2
+  sleep(1);
 }
 
 void Mpu6050::write8(char reg_addr, char data)
 {
-    char buf[2] = {reg_addr, data};
-    this->bus->write(this->slave_addr, 2, buf);
+  char buf[2] = {reg_addr, data};
+  this->bus->write(this->slave_addr, 2, buf);
 }
 
 uint8_t Mpu6050::read8(char reg_addr)
 {
-    char send_buf[1] = {reg_addr};
-    char recv_buf[1];
-    this->bus->write_read(this->slave_addr, 1, send_buf, 1, recv_buf);
-    return (uint8_t) recv_buf[0];
+  char send_buf[1] = {reg_addr};
+  char recv_buf[1];
+  this->bus->write_read(this->slave_addr, 1, send_buf, 1, recv_buf);
+  return (uint8_t) recv_buf[0];
 }
 
 short Mpu6050::read16(char reg_addr)
 {
-    char send_buf[1] = {reg_addr};
-    char recv_buf[2];
-    this->bus->write_read(this->slave_addr, 1, send_buf, 2, recv_buf);
-    return (short) ((recv_buf[0] << 8) | recv_buf[1]); //little-endian
+  char send_buf[1] = {reg_addr};
+  char recv_buf[2];
+  this->bus->write_read(this->slave_addr, 1, send_buf, 2, recv_buf);
+  return (short) ((recv_buf[0] << 8) | recv_buf[1]); //little-endian
+}
+
+uint8_t* Mpu6050::read_bytes(char reg_addr, short length)
+{
+  char send_buf[1] = {reg_addr};
+  char recv_buf[length];
+  this->bus->write_read(this->slave_addr, 1, send_buf, length, recv_buf);
+  return (uint8_t*) recv_buf;
 }
 
 
 
-int Mpu6050::get_raw_accl_x()
+short Mpu6050::get_raw_accl_x()
 {
   return this->read16(0x3B);
 }
 
-int Mpu6050::get_raw_accl_y()
+short Mpu6050::get_raw_accl_y()
 {
   return this->read16(0x3D);
 }
 
-int Mpu6050::get_raw_accl_z()
+short Mpu6050::get_raw_accl_z()
 {
   return this->read16(0x3F);
 }
 
-int Mpu6050::get_raw_gyro_x()
+short Mpu6050::get_raw_gyro_x()
 {
   return this->read16(0x43);
 }
 
-int Mpu6050::get_raw_gyro_y()
+short Mpu6050::get_raw_gyro_y()
 {
   return this->read16(0x45);
 }
 
-int Mpu6050::get_raw_gyro_z()
+short Mpu6050::get_raw_gyro_z()
 {
   return this->read16(0x47);
 }
 
-int main()
+RawSensorData Mpu6050::get_raw_sensor_data()
 {
-    Mpu6050 sensor(new I2C);
-    for (int i = 0; i < 10000; i++)
-    {
-        printf("AX=%7d, AY=%7d, AZ=%7d, GX=%7d, GY=%7d, GZ=%7d\n",
-            sensor.get_raw_accl_x(),
-            sensor.get_raw_accl_y(),
-            sensor.get_raw_accl_z(),
-            sensor.get_raw_gyro_x(),
-            sensor.get_raw_gyro_y(),
-            sensor.get_raw_gyro_z());
-    }
+  uint8_t *bytes = this->read_bytes(0x3B, 14); //array of length 14
+  RawSensorData data;
+  data.accl.x = (short) ((bytes[0] << 8) | bytes[1]);
+  data.accl.y = (short) ((bytes[2] << 8) | bytes[3]);
+  data.accl.z = (short) ((bytes[4] << 8) | bytes[5]);
+  data.temp = (short) ((bytes[6] << 8) | bytes[7]);
+  data.gyro.x = (short) ((bytes[8] << 8) | bytes[9]);
+  data.gyro.y = (short) ((bytes[10] << 8) | bytes[11]);
+  data.gyro.z = (short) ((bytes[12] << 8) | bytes[13]);
+  return data;
 }
--- drivers/mpu6050.hpp
+++ drivers/mpu6050.hpp
@@ -1,23 +1,39 @@
-#include <cstdlib>
-#include <cstdio>
+#include <cstdint>
 #include "i2c.hpp"
 
 const unsigned char DEFAULT_SLAVE_ADDR = 0x68;
 
+typedef struct {
+  short x, y, z;
+} RawAcclData;
+
+typedef struct {
+  short x, y, z;
+} RawGyroData;
+
+typedef struct {
+  RawAcclData accl;
+  short temp;
+  RawGyroData gyro;
+} RawSensorData;
+
 class Mpu6050{
-    public:
-        Mpu6050 (I2C* bus);
-        int get_raw_accl_x();
-        int get_raw_accl_y();
-        int get_raw_accl_z();
-        int get_raw_gyro_x();
-        int get_raw_gyro_y();
-        int get_raw_gyro_z();
-    private:
-        I2C* bus;
-        uint8_t slave_addr = DEFAULT_SLAVE_ADDR;
-        void init();
-        void write8(char reg_addr, char data);
-        uint8_t read8(char reg_addr);
-        short read16(char reg_addr);
+ public:
+  Mpu6050 (I2C* bus);
+  short get_raw_accl_x();
+  short get_raw_accl_y();
+  short get_raw_accl_z();
+  short get_raw_gyro_x();
+  short get_raw_gyro_y();
+  short get_raw_gyro_z();
+  RawSensorData get_raw_sensor_data();
+
+ private:
+  I2C* bus;
+  uint8_t slave_addr = DEFAULT_SLAVE_ADDR;
+  void init();
+  void write8(char reg_addr, char data);
+  uint8_t read8(char reg_addr);
+  short read16(char reg_addr);
+  uint8_t* read_bytes(char reg_addr, short length);
 };
