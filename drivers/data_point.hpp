fe71383ed2cc062678482b0dd04f35156527ba7a
Brano <b.pilnan@gmail.com>
2017-06-09 16:14:32 +0000

    DataPoint class template for timestamping and integrating quantities

---
+++ drivers/data_point.hpp
@@ -0,0 +1,24 @@
+#ifndef DATAPOINT_H_
+#define DATAPOINT_H_
+
+template <typename T>
+class DataPoint
+{
+  public:
+    double timestamp;
+    T value;
+
+    DataPoint() {}
+    DataPoint(double timestamp, T value) : timestamp(timestamp), value(value) {}
+
+    static DataPoint<T> integrate(const DataPoint<T>& dp0, const DataPoint<T>& dp);
+};
+
+template <typename T>
+DataPoint<T> DataPoint<T>::integrate(const DataPoint<T>& dp0, const DataPoint<T>& dp)
+{
+  return DataPoint(dp.timestamp, (dp.timestamp - dp0.timestamp) * (dp.value + dp0.value) / 2.0);
+}
+
+
+#endif //DATAPOINT_H_
