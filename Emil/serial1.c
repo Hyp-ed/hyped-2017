f776239c3ed658961088c7c4d3f1823826cbff97
Emil Hansen <j.emil.hansen@hotmail.com>
2017-07-21 20:54:01 +0100

    a

--- Emil/serial1.c
+++ Emil/serial1.c
@@ -33,44 +33,61 @@
 
 int main ()
 {
-  int fd ;
- // int count ;
-  //unsigned int nextTime ;
+ //  int fd ;
+ // // int count ;
+ //  //unsigned int nextTime ;
 
-  if ((fd = serialOpen ("/dev/ttyACM0", 115200)) < 0)
+ //  if ((fd = serialOpen ("/dev/ttyACM0", 115200)) < 0)
+ //  {
+ //    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+ //    return 1 ;
+ //  }
+
+ //  if (wiringPiSetup () == -1)
+ //  {
+ //    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+ //    return 1 ;
+ //  }
+
+ //  // nextTime = millis () + 300 ;
+
+ //  // for (count = 0 ; count < 256 ; )
+ //  // {
+ //  //   if (millis () > nextTime)
+ //  //   {
+ //  //     printf ("\nOut: %3d: ", count) ;
+ //  //     fflush (stdout) ;
+ //  //    // serialPutchar (fd, count) ;
+ //  //     nextTime += 300 ;
+ //  //     ++count ;
+ //  //   }
+
+ //  //   delay (3) ;
+
+ //    while (serialDataAvail (fd))
+ //    {
+ //      printf (" -> %3d", serialGetchar (fd)) ;
+ //      fflush (stdout) ;
+ //    }
+
+
+ //  printf ("\n") ;
+
+
+  FILE *tty = fopen("/dev/ttyACM0","r");
+
+
+int a=0;
+  while(1)
   {
-    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-    return 1 ;
+    fscanf(tty,"%d",&a);
+
+    printf("%d\n", a);
+
+
+
+
   }
-
-  if (wiringPiSetup () == -1)
-  {
-    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
-    return 1 ;
-  }
-
-  // nextTime = millis () + 300 ;
-
-  // for (count = 0 ; count < 256 ; )
-  // {
-  //   if (millis () > nextTime)
-  //   {
-  //     printf ("\nOut: %3d: ", count) ;
-  //     fflush (stdout) ;
-  //    // serialPutchar (fd, count) ;
-  //     nextTime += 300 ;
-  //     ++count ;
-  //   }
-
-  //   delay (3) ;
-
-    while (serialDataAvail (fd))
-    {
-      printf (" -> %3d", serialGetchar (fd)) ;
-      fflush (stdout) ;
-    }
-
-
-  printf ("\n") ;
+  fclose(tty);
   return 0 ;
 }
