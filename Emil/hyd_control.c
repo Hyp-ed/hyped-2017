3373293dd2123d3bd8a3c4ac6b7fa771d4191e49
Emil Hansen <j.emil.hansen@hotmail.com>
2017-07-21 09:45:23 +0100

    Test case added

--- Emil/hyd_control.c
+++ Emil/hyd_control.c
@@ -18,8 +18,8 @@
 #define PUMP 7	//pin 4
 
 
-#define pumpSpinup 200 //Spinup time for the pump when runing off load. This will occur before every operation that uses the pump, plan timing accordingly
-#define accumulatorChargingTime 20000 //Time that the accumulators will charge up SHOULD BE REPLACED BY A FUNCTION ONCE THE PRESSURE SENSOR IS WORKING
+#define pumpSpinup 400 //Spinup time for the pump when runing off load. This will occur before every operation that uses the pump, plan timing accordingly
+#define accumulatorChargingTime 2000 //Time that the accumulators will charge up SHOULD BE REPLACED BY A FUNCTION ONCE THE PRESSURE SENSOR IS WORKING
 //global functions definitions
 void hold_one(); //Holds brake 1 in place without affecting the operations of brake 2
 void hold_two(); //Holds brake 2 in place without affecting the operations of brake 1
@@ -31,7 +31,10 @@ void standby(); //Shuts down the pump and holds both cylinders in place
 void shutDown(); //Shuts down the pump and releases both cylinders
 void chargeAccumulators(); //Charges the accumulators 
 void test(); // Turns on values one by one to check that all solenoids click and are correctly mapped. (Battery can be connected for this operation but doesn't have to)
+void testCase(); //Runs through various testing scenarios of the hydraulics, begins by charging the accumulators to the appropriate pressure.
+
 //main accepts an argument from the command line
+
 
 int main ( int argc, char *argv[] )
 {
@@ -101,81 +104,21 @@ standby();
 return 0 ;
 
 }
-
-void test()
+void testCase()
 {
-    printf("\nTurning all solenoids off\n");
-    digitalWrite(SOL_1, HIGH);
-    digitalWrite(SOL_2, HIGH);
-    digitalWrite(SOL_3, HIGH);
-    digitalWrite(SOL_4, HIGH);
-    digitalWrite(SOL_5, HIGH);
-    digitalWrite(SOL_6, HIGH);
-    digitalWrite(SOL_7, HIGH);
-    digitalWrite(PUMP, HIGH);
-    printf("\nPUMP HIGH\n");
-    digitalWrite(PUMP, LOW);
-    delay(1000);
-    digitalWrite(PUMP, LOW);
-    delay(1000);
-    printf("\nturns on all solenoids to test which work\n");
-    printf("\n1 HIGH\n");
-    digitalWrite(SOL_1, LOW);
-    delay(1000);
-    printf("\n1 LOW\n");
-    digitalWrite(SOL_1, HIGH);
-    delay(1000);
-
-    printf("\n2 HIGH\n");
-    digitalWrite(SOL_2, LOW);
-    delay(1000);
-    printf("\n2 LOW\n");
-    digitalWrite(SOL_2, HIGH);
-    delay(1000);
-
-    printf("\n3 HIGH\n");
-    digitalWrite(SOL_3, LOW);
-    delay(1000);
-    printf("\n3 LOW\n");
-    digitalWrite(SOL_3, HIGH);
-    delay(1000);
-
-    printf("\n4 HIGH\n");
-    digitalWrite(SOL_4, LOW);
-    delay(1000);
-    printf("\n4 LOW\n");
-    digitalWrite(SOL_4, HIGH);
-    delay(1000);
-
-    printf("\n5 HIGH\n");
-    digitalWrite(SOL_5, LOW);
-    delay(1000);
-    printf("\n5 LOW\n");
-    digitalWrite(SOL_5, HIGH);
-    delay(1000);
-
-    printf("\n6 HIGH\n");
-    digitalWrite(SOL_6, LOW);
-    delay(1000);
-    printf("\n6 LOW\n");
-    digitalWrite(SOL_6, HIGH);
-    delay(1000);
-
-    printf("\n7 HIGH\n");
-    digitalWrite(SOL_7, LOW);
-    delay(1000);
-    printf("\n7 LOW\n");
-    digitalWrite(SOL_7, HIGH);
-    delay(1000);
-
-  
-
-
-
-
-
-
+    printf("\nRUNNING TEST CASE 1\n")
+    chargeAccumulators();
+    delay (2000);
+    retract_one();
+    retract_two();
+    delay(500);
+    standby();
+    extend_one();
+    extend_two();
+    delay(500)
+    standby();
 }
+
 
 void hold_one()
 {
@@ -241,9 +184,6 @@ void shutDown()
     digitalWrite(PUMP, HIGH);
     delay(1000);
     printf("\nProgram Complete\nSAFE TO DISCHARGE ACCUMULATORS\n");
-
-
-
 }
 
 void standby()
@@ -280,4 +220,71 @@ delay(accumulatorChargingTime);
     digitalWrite(SOL_6, HIGH);
     digitalWrite(SOL_7, HIGH);
     digitalWrite(PUMP, HIGH);
+    printf("\nACCUMULATORS CHARGED!\n")
+}
+void test()
+{
+    printf("\nTurning all solenoids off\n");
+    digitalWrite(SOL_1, HIGH);
+    digitalWrite(SOL_2, HIGH);
+    digitalWrite(SOL_3, HIGH);
+    digitalWrite(SOL_4, HIGH);
+    digitalWrite(SOL_5, HIGH);
+    digitalWrite(SOL_6, HIGH);
+    digitalWrite(SOL_7, HIGH);
+    digitalWrite(PUMP, HIGH);
+    printf("\nPUMP HIGH\n");
+    digitalWrite(PUMP, LOW);
+    delay(1000);
+    digitalWrite(PUMP, LOW);
+    delay(1000);
+    printf("\nturns on all solenoids to test which work\n");
+    printf("\n1 HIGH\n");
+    digitalWrite(SOL_1, LOW);
+    delay(1000);
+    printf("\n1 LOW\n");
+    digitalWrite(SOL_1, HIGH);
+    delay(1000);
+
+    printf("\n2 HIGH\n");
+    digitalWrite(SOL_2, LOW);
+    delay(1000);
+    printf("\n2 LOW\n");
+    digitalWrite(SOL_2, HIGH);
+    delay(1000);
+
+    printf("\n3 HIGH\n");
+    digitalWrite(SOL_3, LOW);
+    delay(1000);
+    printf("\n3 LOW\n");
+    digitalWrite(SOL_3, HIGH);
+    delay(1000);
+
+    printf("\n4 HIGH\n");
+    digitalWrite(SOL_4, LOW);
+    delay(1000);
+    printf("\n4 LOW\n");
+    digitalWrite(SOL_4, HIGH);
+    delay(1000);
+
+    printf("\n5 HIGH\n");
+    digitalWrite(SOL_5, LOW);
+    delay(1000);
+    printf("\n5 LOW\n");
+    digitalWrite(SOL_5, HIGH);
+    delay(1000);
+
+    printf("\n6 HIGH\n");
+    digitalWrite(SOL_6, LOW);
+    delay(1000);
+    printf("\n6 LOW\n");
+    digitalWrite(SOL_6, HIGH);
+    delay(1000);
+
+    printf("\n7 HIGH\n");
+    digitalWrite(SOL_7, LOW);
+    delay(1000);
+    printf("\n7 LOW\n");
+    digitalWrite(SOL_7, HIGH);
+    delay(1000);
 }
