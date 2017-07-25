29453f263b04ae56a46dff83cdb485b42492f5c6
Brano <b.pilnan@gmail.com>
2017-07-25 07:59:01 +0000

    formatting and member initialization

--- drivers/data_point.hpp
+++ drivers/data_point.hpp
@@ -8,16 +8,21 @@ class DataPoint
     double timestamp;
     T value;
 
-    DataPoint() {}
-    DataPoint(double timestamp, T value) : timestamp(timestamp), value(value) {}
+    DataPoint() : timestamp(0.0)
+    {}
+    DataPoint(double timestamp, T value) : timestamp(timestamp), value(value)
+    {}
 
-    static DataPoint<T> integrate(const DataPoint<T>& dp0, const DataPoint<T>& dp);
+    static DataPoint<T> integrate(const DataPoint<T>& dp0,
+        const DataPoint<T>& dp);
 };
 
 template <typename T>
-DataPoint<T> DataPoint<T>::integrate(const DataPoint<T>& dp0, const DataPoint<T>& dp)
+DataPoint<T> DataPoint<T>::integrate(const DataPoint<T>& dp0,
+    const DataPoint<T>& dp)
 {
-  return DataPoint(dp.timestamp, (dp.timestamp - dp0.timestamp) * (dp.value + dp0.value) / 2.0);
+  return DataPoint(dp.timestamp,
+      (dp.timestamp - dp0.timestamp) * (dp.value + dp0.value) / 2.0);
 }
 
 
