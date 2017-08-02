29a5c4f4422fae49f547f848e17532412538010f
Emil Hansen <j.emil.hansen@hotmail.com>
2017-08-02 12:10:47 +0100

    Hydraulics file adjusted to take frontrear and leftright arguments.

--- Hydraulics/hydraulics.c
+++ Hydraulics/hydraulics.c
@@ -17,29 +17,109 @@ Check the processing power that this will use and if it is too high, add a delay
 #define overpressurePump 200;
 
 
-void hold_one()
+void hold(frontrear, leftright)
 {
-    digitalWrite(SOL_2, LOW);
+if (strcpm(frontrear, "front") == 0 )
+{
+    int SOL_1 = SOL_1_F;
+    int SOL_2 = SOL_2_F;
+    int SOL_3 = SOL_3_F;
+    int SOL_4 = SOL_4_F;
+    int SOL_5 = SOL_5_F;
+    int SOL_6 = SOL_6_F;
+    int SOL_7 = SOL_7_F;
+    int PUMP = PUMP_F;
+}
+else if (strcpm(frontrear, "rear")== 0 )
+{
+    int SOL_1 = SOL_1_R;
+    int SOL_2 = SOL_2_R;
+    int SOL_3 = SOL_3_R;
+    int SOL_4 = SOL_4_R;
+    int SOL_5 = SOL_5_R;
+    int SOL_6 = SOL_6_R;
+    int SOL_7 = SOL_7_R;
+    int PUMP = PUMP_R;
+}
+else 
+{
+    printf("invalid input");
+    return
+}
+
+
+if (strcpm(leftright, "right")== 0)
+{
+
+
+    digitalWrite(SOL_2, HIGH);
     digitalWrite(SOL_1, HIGH);
-    safetyCheck();
 }
-
-void hold_two()
+else if (strcpm(leftright, "left")== 0)
 {
-    digitalWrite(SOL_6, LOW);
+
+
+    digitalWrite(SOL_6, HIGH);
     digitalWrite(SOL_4, HIGH);
+}
+else
+{
+    putchar("invalid input");
+    return
+}
     safetyCheck();
 }
 
 
-void retract_one()
+
+
+
+
+
+
+
+void retract(frontrear, leftright)
 {
+   if (strcpm(frontrear, "front") == 0 )
+{
+    int SOL_1 = SOL_1_F;
+    int SOL_2 = SOL_2_F;
+    int SOL_3 = SOL_3_F;
+    int SOL_4 = SOL_4_F;
+    int SOL_5 = SOL_5_F;
+    int SOL_6 = SOL_6_F;
+    int SOL_7 = SOL_7_F;
+    int PUMP = PUMP_F;
+}
+else if (strcpm(frontrear, "rear")== 0 )
+{
+    int SOL_1 = SOL_1_R;
+    int SOL_2 = SOL_2_R;
+    int SOL_3 = SOL_3_R;
+    int SOL_4 = SOL_4_R;
+    int SOL_5 = SOL_5_R;
+    int SOL_6 = SOL_6_R;
+    int SOL_7 = SOL_7_R;
+    int PUMP = PUMP_R;
+}
+else 
+{
+    printf("invalid input");
+    return
+}
+
+
     digitalWrite(PUMP, LOW);
     delay(50);
     while (getData("current")> inrushLimit)
     {
         delay(50);
     }
+
+
+if (strcpm(leftright, "right")== 0)
+{
+
     digitalWrite(SOL_1, LOW);
     digitalWrite(SOL_2, LOW);
     digitalWrite(SOL_3, HIGH);
@@ -47,41 +127,118 @@ void retract_one()
     digitalWrite(SOL_5, LOW);
     digitalWrite(SOL_6, HIGH);
     digitalWrite(SOL_7, LOW);
+}
+else if (strcpm(leftright, "left")== 0)
+{
+
+
+      digitalWrite(SOL_1, LOW);
+    digitalWrite(SOL_2, LOW);
+    digitalWrite(SOL_3, HIGH);
+    digitalWrite(SOL_4, HIGH);
+    digitalWrite(SOL_5, LOW);
+    digitalWrite(SOL_6, HIGH);
+    digitalWrite(SOL_7, LOW);  //NEEDS TO BE UPDATED WITH HE RIGHT SOLENOIDS
+}
+else
+{
+    putchar("invalid input");
+    return
+}
+
  
 
 }
 
-void retract_two()
+
+
+
+
+
+
+
+void extend(frontrear, leftright)
 {
-        digitalWrite(PUMP, LOW);
-        delay(50);
-    while (getData("current")> inrushLimit)
-    {
-        delay(50);
-    }
-    digitalWrite(SOL_1, HIGH);
-    digitalWrite(SOL_2, HIGH);
-    digitalWrite(SOL_3, HIGH);
-    digitalWrite(SOL_4, LOW);
-    digitalWrite(SOL_5, LOW);
-    digitalWrite(SOL_6, LOW);
-    digitalWrite(SOL_7, LOW);
-    
+    if (strcpm(frontrear, "front") == 0 )
+{
+    int SOL_1 = SOL_1_F;
+    int SOL_2 = SOL_2_F;
+    int SOL_3 = SOL_3_F;
+    int SOL_4 = SOL_4_F;
+    int SOL_5 = SOL_5_F;
+    int SOL_6 = SOL_6_F;
+    int SOL_7 = SOL_7_F;
+    int PUMP = PUMP_F;
+}
+else if (strcpm(frontrear, "rear")== 0 )
+{
+    int SOL_1 = SOL_1_R;
+    int SOL_2 = SOL_2_R;
+    int SOL_3 = SOL_3_R;
+    int SOL_4 = SOL_4_R;
+    int SOL_5 = SOL_5_R;
+    int SOL_6 = SOL_6_R;
+    int SOL_7 = SOL_7_R;
+    int PUMP = PUMP_R;
+}
+else 
+{
+    printf("invalid input");
+    return
 }
 
-void extend_one()
+if (strcpm(leftright, "right")== 0)
 {
+
+
     digitalWrite(SOL_2, HIGH);
     digitalWrite(SOL_1, HIGH);
 }
-void extend_two()
+else if (strcpm(leftright, "left")== 0)
 {
+
+
     digitalWrite(SOL_6, HIGH);
     digitalWrite(SOL_4, HIGH);
 }
-
-void shutDown()
+else
 {
+    putchar("invalid input");
+    return
+}
+}
+
+
+
+void shutDown(frontrear)
+{
+    if (strcpm(frontrear, "front") == 0 )
+{
+    int SOL_1 = SOL_1_F;
+    int SOL_2 = SOL_2_F;
+    int SOL_3 = SOL_3_F;
+    int SOL_4 = SOL_4_F;
+    int SOL_5 = SOL_5_F;
+    int SOL_6 = SOL_6_F;
+    int SOL_7 = SOL_7_F;
+    int PUMP = PUMP_F;
+}
+else if (strcpm(frontrear, "rear")== 0 )
+{
+    int SOL_1 = SOL_1_R;
+    int SOL_2 = SOL_2_R;
+    int SOL_3 = SOL_3_R;
+    int SOL_4 = SOL_4_R;
+    int SOL_5 = SOL_5_R;
+    int SOL_6 = SOL_6_R;
+    int SOL_7 = SOL_7_R;
+    int PUMP = PUMP_R;
+}
+else 
+{
+    printf("invalid input");
+    return
+}
     digitalWrite(SOL_1, HIGH);
     digitalWrite(SOL_2, HIGH);
     digitalWrite(SOL_3, HIGH);
@@ -96,8 +253,35 @@ void shutDown()
     printf("Accumulator pressure is: %f\n", getData("accumulator_pressure"));
 }
 
-void standby()
+void standby(frontrear)
 {
+    if (strcpm(frontrear, "front") == 0 )
+{
+    int SOL_1 = SOL_1_F;
+    int SOL_2 = SOL_2_F;
+    int SOL_3 = SOL_3_F;
+    int SOL_4 = SOL_4_F;
+    int SOL_5 = SOL_5_F;
+    int SOL_6 = SOL_6_F;
+    int SOL_7 = SOL_7_F;
+    int PUMP = PUMP_F;
+}
+else if (strcpm(frontrear, "rear")== 0 )
+{
+    int SOL_1 = SOL_1_R;
+    int SOL_2 = SOL_2_R;
+    int SOL_3 = SOL_3_R;
+    int SOL_4 = SOL_4_R;
+    int SOL_5 = SOL_5_R;
+    int SOL_6 = SOL_6_R;
+    int SOL_7 = SOL_7_R;
+    int PUMP = PUMP_R;
+}
+else 
+{
+    printf("invalid input");
+    return
+}
     digitalWrite(SOL_1, HIGH);
     digitalWrite(SOL_2, LOW);
     digitalWrite(SOL_3, HIGH);
@@ -109,13 +293,38 @@ void standby()
     safetyCheck();
 }
 
-void chargeAccumulators(pressure_nominal_accumulator) //Takes input argument of the prechage pressure that the accumulators should have. Charges the accumulators until this value is reached.
+void chargeAccumulators(frontrear, pressure_nominal_accumulator) //Takes input argument of the prechage pressure that the accumulators should have. Charges the accumulators until this value is reached.
 {
-int pressure_accumulator=0;
-	pressure_read_accumulator();
+    if (strcpm(frontrear, "front") == 0 )
+{
+    int SOL_1 = SOL_1_F;
+    int SOL_2 = SOL_2_F;
+    int SOL_3 = SOL_3_F;
+    int SOL_4 = SOL_4_F;
+    int SOL_5 = SOL_5_F;
+    int SOL_6 = SOL_6_F;
+    int SOL_7 = SOL_7_F;
+    int PUMP = PUMP_F;
+}
+else if (strcpm(frontrear, "rear")== 0 )
+{
+    int SOL_1 = SOL_1_R;
+    int SOL_2 = SOL_2_R;
+    int SOL_3 = SOL_3_R;
+    int SOL_4 = SOL_4_R;
+    int SOL_5 = SOL_5_R;
+    int SOL_6 = SOL_6_R;
+    int SOL_7 = SOL_7_R;
+    int PUMP = PUMP_R;
+}
+else 
+{
+    printf("invalid input");
+    return
+}
  
 printf("\nCHARGING ACCUMULATORS\n");   
-while(pressure_accumulator < pressure_nominal_accumulator) {
+while(getData("pressure_accumulator")< pressure_nominal_accumulator) {
     safetyCheck();
     digitalWrite(PUMP, LOW);
     delay(50);
@@ -130,8 +339,8 @@ while(pressure_accumulator < pressure_nominal_accumulator) {
     digitalWrite(SOL_5, HIGH);
     digitalWrite(SOL_6, HIGH);
     digitalWrite(SOL_7, LOW);
-    pressure_read_accumulator();
 }
+
 
     digitalWrite(SOL_1, HIGH);
     digitalWrite(SOL_2, HIGH);
--- Hydraulics/hydraulics.h
+++ Hydraulics/hydraulics.h
@@ -9,25 +9,35 @@
 #include "serialData.h"
 
 
-#define SOL_1 0    //physical pin 17
-#define SOL_2 1 //physical pin 18
-#define SOL_3 2 //pin 21
-#define SOL_4 3         //pin 22
-#define SOL_5 4 //pin 23
-#define SOL_6 5 //pin 24
-#define SOL_7 6 //pin 25
-#define PUMP 7  //pin 4
+#define SOL_1_F 0    //physical pin 17
+#define SOL_2_F 1 //physical pin 18
+#define SOL_3_F 2 //pin 21
+#define SOL_4_F 3         //pin 22
+#define SOL_5_F 4 //pin 23
+#define SOL_6_F 5 //pin 24
+#define SOL_7_F 6 //pin 25
+#define PUMP_F 7  //pin 4
 
-void hold_one(); //Holds brake 1 in place without affecting the operations of brake 2
-void hold_two(); //Holds brake 2 in place without affecting the operations of brake 1
-void retract_one(); //Retracts brake 1 without affecting the operations of brake 2
-void retract_two(); //Retracts brake 2 without affecting the operations of brake 1
-void extend_one(); //Extends brake 1 without affecting the operations of brake 2 RELIES ON T$
-void extend_two(); //Extends brake 2 without affecting the operations of brake 1 RELIES ON T$
-void standby(); //Shuts down the pump and holds both cylinders in place
-void shutDown(); //Shuts down the pump and releases both cylinders
-void chargeAccumulators(); //Charges the accumulators
-void startUp();// Retracts brakes and charges accumulators
+#define SOL_1_R 0    //physical pin 17
+#define SOL_2_R 1 //physical pin 18
+#define SOL_3_R 2 //pin 21
+#define SOL_4_R 3         //pin 22
+#define SOL_5_R 4 //pin 23
+#define SOL_6_R 5 //pin 24
+#define SOL_7_R 6 //pin 25
+#define PUMP_R 7  //pin 4
+
+
+
+
+
+void hold(frontrear, leftright); //Holds brake 1 in place without affecting the operations of brake 2
+void retract(frontrear, leftright); //Retracts brake 1 without affecting the operations of brake 2
+void extend(frontrear, leftright); //Extends brake 1 without affecting the operations of brake 2 RELIES ON T$
+void standby(frontrear); //Shuts down the pump and holds both cylinders in place
+void shutDown(frontrear); //Shuts down the pump and releases both cylinders
+void chargeAccumulators(frontrear, pressure_nominal_accumulator); //Charges the accumulators
+void startUp(frontrear);// Retracts brakes and charges accumulators
 void safetyCheck(); //Checks that all values are within the nominal range. Checks pump pressure, accumulator pressure, battery voltages and current draw, if any exceed nominal ranges, emergeny procedure will begin.
 
 
