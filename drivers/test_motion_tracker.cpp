52c283be5038d1c97ce915c10f55c85fb19a77a2
Brano <b.pilnan@gmail.com>
2017-08-04 10:22:05 +0000

    Upgraded test_motion_tracker's UI; file output not yet implemented

--- drivers/test_motion_tracker.cpp
+++ drivers/test_motion_tracker.cpp
@@ -1,7 +1,7 @@
-#include <ncurses.h>
-#include <thread>
 #include <chrono>
 #include <cmath>
+#include <ncurses.h>
+#include <thread>
 
 #include "i2c.hpp"
 #include "mpu6050.hpp"
@@ -9,7 +9,19 @@
 #include "quaternion.hpp"
 #include "vector3d.hpp"
 
-const double PI = 3.1415926535897932;
+#define GYRO_POS 5
+#define ACCL_POS 9 
+
+bool two_imus;
+I2C i2c;
+Mpu6050 imu1(&i2c);
+Mpu6050* imu2 = nullptr;
+MotionTracker mt;
+
+void setup();
+void finalize();
+void file_output();
+void screen_output();
 
 int main()
 {
@@ -18,7 +30,38 @@ int main()
   noecho();
   timeout(0);
 
-  mvprintw(0, 0, "Angular velocity = " );
+  mvprintw(0, 0, "Enter the number of MPU6050 sensors to be used (1 or 2)");
+  move(1, 0);
+  refresh();
+  int opt = getch();
+  while(opt != '1' && opt != '2')
+    opt = getch();
+  two_imus = (opt == '2');
+  mvaddch(1, 0, opt);
+  refresh();
+
+  mvprintw(2, 0, "Press 'f' for output to file or 's' for output on screen");
+  move(3, 0);
+  refresh();
+  opt = getch();
+  while(opt != 'f' && opt != 's')
+    opt = getch();
+  mvaddch(3, 0, opt);
+  move(4, 0);
+  refresh();
+  if (opt == 'f')
+  {
+    endwin();
+    file_output();
+  }
+  else
+  {
+    screen_output();
+    endwin();
+  }
+  finalize();//*/
+
+  /*mvprintw(0, 0, "Angular velocity = " );
   mvprintw(1, 0, "Rotor = ");
   mvprintw(2, 0, "R*k*R^-1 = ( 0.000,  0.000,  1.000)   Angle: ");
   mvprintw(4, 0, "                 x          y          z          total");
@@ -38,35 +81,75 @@ int main()
   mvprintw(9, 0, "Type any character to exit");
   move(10, 0);
   refresh();
+  mt.stop();
+
+  //getch();
+  endwin();//*/
+}
+
+void setup()
+{
+  mt.add_imu(imu1);
+  if (two_imus)
+  {
+    imu2 = new Mpu6050(&i2c, ALTERNATIVE_SLAVE_ADDR);
+    mt.add_imu(*imu2);
+  }
+  mt.start();
+}
+
+void finalize()
+{
+  mt.stop();
+  if (two_imus)
+    delete imu2;
+}
+
+void screen_output()
+{
+  mvprintw(GYRO_POS, 0, "Calibrating, please wait...");
+  move(GYRO_POS + 1, 0);
+  refresh();
+  setup();
+  mvprintw(ACCL_POS + 5, 0, "Type any character to exit");
+  mvprintw(ACCL_POS, 0,
+      "                    x             y             z             total");
+  move(ACCL_POS + 6, 0);
+  refresh();
   int stop = getch();
   while (stop == ERR)
   {
     Vector3D<double> v = mt.get_angular_velocity();
-    mvprintw(0, 19, "(%10.6f,  %10.6f,  %10.6f)", v.x, v.y, v.z);
+    mvprintw(GYRO_POS, 0, "Angular velocity: (%10.6f,  %10.6f,  %10.6f)",
+        v.x, v.y, v.z);
     Quaternion r = mt.get_rotor();
-    mvprintw(1, 8, "(%12.6f, %12.6f, %12.6f, %12.6f)",
-        r.scal, r.vect.x, r.vect.y, r.vect.z);
+    mvprintw(GYRO_POS + 1, 0,
+        "Rotor: (%12.6f, %12.6f, %12.6f, %12.6f)   Norm: %12.6f",
+        r.scal, r.vect.x, r.vect.y, r.vect.z, Quaternion::norm(r));
     Vector3D<double> up(0,0,1);
     up = r*up*Quaternion::inv(r);
-    mvprintw(2, 0, "R*k*R^-1 = (%6.3f, %6.3f, %6.3f)   Angle: %6.3f",
-        up.x, up.y, up.z, acos(up.z / Quaternion::norm(up)) * 180.0 / PI);
+    double norm = Quaternion::norm(up);
+    mvprintw(GYRO_POS + 2, 0,
+        "R*k*R^-1 = (%6.3f, %6.3f, %6.3f)   Norm:%6.3f   Angle: %6.3f",
+        up.x, up.y, up.z, norm, acos(up.z / norm) * 180.0 / PI);
 
     v = mt.get_acceleration();
-    mvprintw(5, 14, "%12.6f  %12.6f  %12.6f   %12.6f", v.x, v.y, v.z,
-        Quaternion::norm(v));
+    mvprintw(ACCL_POS + 1, 0, "Acceleration  %12.6f  %12.6f  %12.6f   %12.6f",
+        v.x, v.y, v.z, Quaternion::norm(v));
     v = mt.get_velocity();
-    mvprintw(6, 14, "%12.6f  %12.6f  %12.6f   %12.6f", v.x, v.y, v.z,
-        Quaternion::norm(v));
+    mvprintw(ACCL_POS + 2, 0, "Velocity      %12.6f  %12.6f  %12.6f   %12.6f",
+        v.x, v.y, v.z, Quaternion::norm(v));
     v = mt.get_displacement();
-    mvprintw(7, 14, "%12.6f  %12.6f  %12.6f   %12.6f", v.x, v.y, v.z,
-        Quaternion::norm(v));
-    move(10, 0);
+    mvprintw(ACCL_POS + 3, 0, "Displacement  %12.6f  %12.6f  %12.6f   %12.6f",
+        v.x, v.y, v.z, Quaternion::norm(v));
+    move(ACCL_POS + 6, 0);
     refresh();
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     stop = getch();
   }
-  mt.stop();
-
-  //getch();
-  endwin();
 }
+
+void file_output()
+{
+}
+
