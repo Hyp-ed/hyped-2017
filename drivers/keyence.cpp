f64e9ff03e6c556cc5b935d315e27ec930225afa
Brano <b.pilnan@gmail.com>
2017-08-22 11:28:38 +0000

    Implemented new keyence driver (not tested yet)

--- drivers/keyence.cpp
+++ drivers/keyence.cpp
@@ -1,50 +1,54 @@
 #include "keyence.hpp"
 
 #include <chrono>
-#include <thread>
 
 
 Keyence::Keyence(GpioPinNumber config_pin_num, GpioPinNumber output_pin_num)
     : config_pin { Gpio::get_pin(config_pin_num, PinMode::out, PudControl::off) },
-    output_pin { Gpio::get_pin(output_pin_num, PinMode::in, PudControl::down) }
-{
-	setUp();
-	countStripes(stripes);
-}
+    output_pin { Gpio::get_pin(output_pin_num, PinMode::in, PudControl::down) },
+    count(0), stop_flag(true)
+{}
 
 void Keyence::calibrate()
 {
   this->config_pin.write(false);
-  //:delay(3500);
+  std::this_thread::sleep_for(std::chrono::milliseconds(3500));
   this->config_pin.write(true);
   //printf("\nSetup complete");	
 }
 
-void keyence :: countStripes(int &numStripes)
+int Keyence::get_count()
 {
-	while (1) 
-	{
-		current_state = digitalRead(28);
-		printf("\noutput = %d", current_state);
-		if (current_state == 0 && prev_state == 0)
-		{
-			prev_state = 1;
-			//numStripes += 0;
-			printf("\n Stripe started");
-		}	
-		else if (current_state == 0 && prev_state == 1)
-		{
-			prev_state = 1;
-			printf("\n Stripe still present");
-		}
-		else if (current_state == 1 && prev_state == 1)
-		{
-			prev_state = 0;
-			numStripes += 1;
-			printf("\n Stripe finished. Stripe Count = %d", numStripes);
-		}
-  		//Change delay to vary the reading frequency
-  		delayMicroseconds(1000);
-  		
-	}
+  return this->count.load(std::memory_order_relaxed);
+}
+
+void Keyence::count_stripes()
+{
+  int count = 0;
+  bool current, previous = false;
+  while (!this->stop_flag)
+  {
+    current = this->output_pin.read();
+    //printf("\noutput = %d", current_state);
+    if (current && !previous)
+    {
+      // Stripe started
+      //printf("\n Stripe started");
+    }	
+    if (current && previous)
+    {
+      // Stripe in progress
+      //printf("\n Stripe still present");
+    }
+    if (!current && previous)
+    {
+      ++count;
+      //printf("\n Stripe finished. Stripe Count = %d", count);
+    }
+
+    this->count.store(count, std::memory_order_relaxed);
+    previous = current;
+    //Change delay to vary the reading frequency
+    std::this_thread::sleep_for(std::chrono::microseconds(100));
+  }
 }
--- drivers/keyence.hpp
+++ drivers/keyence.hpp
@@ -1,3 +1,6 @@
+#include <atomic>
+#include <thread>
+
 #include "gpio.hpp"
 
 #define CTL_SIG_PIN 29
@@ -6,14 +9,18 @@
 
 class Keyence
 {
-  private: 
-    bool current_state = false;
-    bool prev_state = false;
-    GpioPin& config_pin;
-    GpioPin& output_pin;
-
   public:
     Keyence(GpioPinNumber config_pin_num, GpioPinNumber output_pin_num);
     void calibrate();
-    void countStripes(int &numStripes);			
+    void start();
+    int get_count();
+  
+  private:
+    void count_stripes();
+
+    GpioPin& config_pin;
+    GpioPin& output_pin;
+    std::atomic<int> count;
+    std::atomic_bool stop_flag;
+    std::thread counting_thread;
 };
