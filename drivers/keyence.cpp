5ff18d3f83323ec82f3ad1d8ef14e1e8f0f4fe59
Brano <b.pilnan@gmail.com>
2017-08-22 11:54:40 +0000

    Testing configuration

--- drivers/keyence.cpp
+++ drivers/keyence.cpp
@@ -1,6 +1,7 @@
 #include "keyence.hpp"
 
 #include <chrono>
+#include <cstdio>
 
 
 Keyence::Keyence(GpioPinNumber config_pin_num, GpioPinNumber output_pin_num)
@@ -14,7 +15,7 @@ void Keyence::calibrate()
   this->config_pin.write(false);
   std::this_thread::sleep_for(std::chrono::milliseconds(3500));
   this->config_pin.write(true);
-  //printf("\nSetup complete");	
+  printf("Setup complete\n");	
 }
 
 int Keyence::get_count()
@@ -29,26 +30,25 @@ void Keyence::count_stripes()
   while (!this->stop_flag)
   {
     current = this->output_pin.read();
-    //printf("\noutput = %d", current_state);
     if (current && !previous)
     {
       // Stripe started
-      //printf("\n Stripe started");
+      printf("Stripe started\n");
     }	
     if (current && previous)
     {
       // Stripe in progress
-      //printf("\n Stripe still present");
+      printf("Stripe still present\n");
     }
     if (!current && previous)
     {
       ++count;
-      //printf("\n Stripe finished. Stripe Count = %d", count);
+      printf("Stripe finished. Stripe Count = %d\n", count);
     }
 
     this->count.store(count, std::memory_order_relaxed);
     previous = current;
     //Change delay to vary the reading frequency
-    std::this_thread::sleep_for(std::chrono::microseconds(100));
+    std::this_thread::sleep_for(std::chrono::microseconds(10000));
   }
 }
