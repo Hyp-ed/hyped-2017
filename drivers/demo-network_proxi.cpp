c9d53df4d0fcafad71ebc1f712001948c9ce9d32
Brano <b.pilnan@gmail.com>
2017-08-25 23:47:15 +0000

    Implemented demo for all proxis of slave4

--- drivers/demo-network_proxi.cpp
+++ drivers/demo-network_proxi.cpp
@@ -6,7 +6,17 @@ int main() {
   NetworkMaster slave1;
   slave1.setup("localhost", 11999);
 
-  NetworkProxi proxi(slave1, "proxi-ground-front");
-  std::cout << proxi.get_distance() << std::endl;
+  NetworkProxi proxi1(slave1, "proxi-gnd-rearski-rear");
+  NetworkProxi proxi3(slave1, "proxi-gnd-rearski-front");
+  NetworkProxi proxi2(slave1, "proxi-cylinder-rear");
+  NetworkProxi proxi4(slave1, "proxi-cylinder-front");
+  NetworkProxi proxi5(slave1, "proxi-gnd-frontski-front");
+  NetworkProxi proxi6(slave1, "proxi-gnd-frontski-rear");
+  std::cout << proxi1.get_distance() << std::endl;
+  std::cout << proxi3.get_distance() << std::endl;
+  std::cout << proxi2.get_distance() << std::endl;
+  std::cout << proxi4.get_distance() << std::endl;
+  std::cout << proxi5.get_distance() << std::endl;
+  std::cout << proxi6.get_distance() << std::endl;
 
 }
